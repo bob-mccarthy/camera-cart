dbc9739d0924fe08beda27c29e9d497e8bcd8652
bob-mccarthy <bmccarthy@college.harvard.edu>
2023-11-27 13:20:46 -0500

    added test for encoder without power

---
+++ simple-encoder-test/simple-encoder-test.ino
@@ -0,0 +1,21 @@
+const int ENCA = 2;
+const int ENCB = 4;
+long position = 0;
+void IRAM_ATTR readEncoder(){
+  int b = digitalRead(ENCB);
+  if (b ==HIGH){
+    position++;
+  }
+  else{
+    position--;
+  }
+}
+
+void setup(){
+  Serial.begin(115200);
+  attachInterrupt(digitalPinToInterrupt(ENCA), readEncoder, RISING);
+}
+
+void loop(){
+  Serial.println(position);
+}
